36dde539af46ff5563e259d3990264c09dc0b27b
Niya <niya.90@hotmail.com>
2019-11-21 10:56:24 +0100

    ok

---
+++ graph-tutorial/ViewModels/FieldValues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace graph_tutorial.ViewModels
+{
+    public class FieldValues
+    {
+        public string Title { get; set; }
+        public DateTime Time { get; set; }
+        public string Description{ get; set; }
+        public string Adress{ get; set; }
+        public List<string> Peoples{ get; set; }
+    }
+}
